a291d201bd593f251ea8759c378c1275722a80af
Sakul Jaruthanaset <miolynet@perfenterprise.com>
2015-11-26 03:25:06 +0700

    Add demo controller in the Login page.
    Signed-off-by: Sakul Jaruthanaset <miolynet@perfenterprise.com>

---
+++ src/DailySoccerSolution/DailySoccerMobile/Scripts/accounts/starter.account.AccountController.ts
@@ -0,0 +1,21 @@
+﻿angular
+    .module('starter.account', [])
+    .controller('AccountCtrl', function ($scope, $location, $timeout) {
+
+        $scope.skipLogin = function (): void {
+            // TODO: Login with guest
+            console.log('Doing Register new guest account');
+            $timeout(function () {
+                $location.path('/matches/todaymatches')
+            }, 1000);
+        };
+
+        $scope.loginWithFacebook = function (): void {
+            // TODO: Login with facebook
+            console.log('Doing connecting to facebook');
+            $timeout(function () {
+                $location.path('/matches/todaymatches')
+            }, 1000);
+        };
+
+    });
