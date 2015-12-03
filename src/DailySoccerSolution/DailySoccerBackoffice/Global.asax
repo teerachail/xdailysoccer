f510149bc69693bc66bf67b8ca018ddc129f9e80
Kritsada Dechawantana <kritsada@perfenterprise.com>
2015-12-03 10:18:29 +0700

    Add Backoffice to project!
    Signed-off-by: Kritsada Dechawantana <kritsada@perfenterprise.com>

---
+++ src/DailySoccerSolution/DailySoccerBackoffice/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DailySoccerBackoffice.MvcApplication" Language="C#" %>
---
+++ src/DailySoccerSolution/DailySoccerBackoffice/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DailySoccerBackoffice.Startup))]
+namespace DailySoccerBackoffice
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
