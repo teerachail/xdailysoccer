d0570092a44e348cabb3a817f37d7d5c4a5d1ca0
Sakul Jaruthanaset <miolynet@perfenterprise.com>
2015-12-10 10:44:40 +0700

    Implement SMS sender by Twilio complete.
    Signed-off-by: Sakul Jaruthanaset <miolynet@perfenterprise.com>

--- src/DailySoccerSolution/DailySoccer.Shared/Shared/Facades/TwilioSMSSender.cs
+++ src/DailySoccerSolution/DailySoccer.Shared/Shared/Facades/TwilioSMSSender.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Twilio;
 
 namespace DailySoccer.Shared.Facades
 {
@@ -10,8 +11,17 @@ namespace DailySoccer.Shared.Facades
     {
         public void Send(string sentToPhoneNumber, string message)
         {
-            // TODO: Send
-            throw new NotImplementedException();
+            const string accountSID = "AC093f1fcab483e85f464f14cff2581837";
+            const string authToken = "86d01be3ddba43100cb5d6641fd3848c";
+            var client = new TwilioRestClient(accountSID, authToken);
+
+            const string SentFromPhoneNumber = "+15165709060";
+            var result = client.SendMessage(SentFromPhoneNumber, sentToPhoneNumber, message);
+
+            if (result.RestException != null)
+            {
+                string errorMessage = result.RestException.Message;
+            }
         }
     }
 }
